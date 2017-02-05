bafbf86983b1b0a578639cd1404377826bd0f109
Unknown <che@10g.pl>
2017-02-05 21:22:50 +0100

    Init crazylab
    generate app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name crazylabApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the crazylabApp
+ */
+angular.module('crazylabApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name crazylabApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the crazylabApp
+ */
+angular.module('crazylabApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
