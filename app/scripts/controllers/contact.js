bfdda13606e889d899651a664d74c68216be2e7f
cHeBeee <che@10g.pl>
2017-02-18 22:56:53 +0100

    add contact

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name crazylabApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the crazylabApp
+ */
+angular.module('crazylabApp')
+  .controller('ContactCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
